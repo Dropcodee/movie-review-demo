d2e2d5e7aabf0048599be903418f0464ab86280c
Dropcodee <dropcodetuts@gmail.com>
2022-06-20 09:34:28 +0100

    coming soon page design💩😪

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Nollywood Movies Ratings and Reward Platform For Avid Fans - MovieQart</title>
+    <meta name="description"
+        content="We know you love watching your favorite actors; we think you should be more involved by rating, commenting and get rewarded for the movies you watch.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon"
+        href="https://mir-s3-cdn-cf.behance.net/project_modules/disp/75269f144806585.62933cebd2035.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('font-flaticon/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dripicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/meanmenu.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+</head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-013K4LNBD8"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-013K4LNBD8');
+</script>
+
+
+
+<body>
+    <!-- header -->
+    <header class="header-area header-three">
+
+        <div id="header-sticky" class="menu-area">
+
+            <div class="container-fluid pl-50 pr-50">
+
+                <div class="second-menu">
+
+                    <div class="row align-items-center">
+
+                        <div class="col-xl-1 col-lg-1">
+
+                            <div class="logo">
+
+                                <a href="https://movieqart.com/">
+
+                                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/dca651144806585.6293d6cc100a6.png"
+                                        alt="MovieQart logo">
+
+                                </a>
+
+                            </div>
+                        </div>
+
+                        <div class="col-xl-2 col-lg-2">
+
+                        </div>
+
+                        <div class="col-xl-7 col-lg-7">
+
+                            <!-- <div class="main-menu">
+                                    <nav id="mobile-menu">
+                                        <ul>
+                                            <li class="has-sub">
+            <a href="#">Home</a>
+
+           </li>
+                                             <li><a href="#">About Us</a></li>
+
+
+                                            <li><a href="#">Blog</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div> -->
+                        </div>
+                        <div class="col-xl-2 col-lg-2 text-left d-none d-lg-block mt-30 mb-30">
+                            <div class="cart-top">
+                                <ul>
+                                    <li><a href="https://www.instagram.com/movieqartng" target="_blank"><i
+                                                class="fab fa-instagram"></i></a></li>
+                                    <li><a href="https://www.facebook.com/movieqart" target="_blank"><i
+                                                class="fab fa-facebook-f"></i></a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- <div class="col-xl-1 col-lg-1 text-right d-none d-lg-block mt-30 mb-30">
+                                <a href="#" class="menu-tigger"><img src="img/bg/toggle-menu.png" alt="logo"></a>
+                            </div> -->
+
+
+                        <div class="col-12">
+                            <div class="mobile-menu"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- main-area -->
+    <main>
+
+        <!-- slider-area -->
+        <section id="home" class="slider-area slider-four fix p-relative">
+
+            <div class="slider-active">
+                <div class="single-slider slider-bg d-flex align-items-center"
+                    style="background: url(https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500) no-repeat;background-size: cover; background-position: center top;">
+                    <div class="container">
+                        <div class="row justify-content-center pt-50">
+                            <!-- <div class="col-lg-1 col-md-1">
+                               </div> -->
+                            <div class="col-lg-9 col-md-9">
+                                <div class="slider-content s-slider-content">
+                                    <h5 data-animation="fadeInDown" data-delay=".4s">Launching Soon</h5>
+                                    <h2 data-animation="fadeInUp" data-delay=".4s">Win Gifts As You Watch Your Favorite
+                                        Nigerian Movies</h2>
+                                    <div class="slider-btn">
+                                        <a href="https://share-eu1.hsforms.com/1ZwFL1WqzSJCxqeql7jtZDwffbny"
+                                            target="_blank" class="btn ss-btn mr-15">Join Waitlist</a>
+
+                                    </div>
+
+
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </section>
+        <!-- slider-area-end -->
+
+
+
+
+
+
+    </main>
+    <!-- main-area-end -->
+    <!-- footer -->
+    <footer class="footer-bg footer-p" style="background: #1e191f;">
+        <div class="footer-top pt-70">
+            <div class="container">
+                <div class="row justify-content-between">
+
+                    <div class="col-xl-4 col-lg-4 col-sm-6">
+                        <div class="footer-widget mb-30">
+                            <div class="f-widget-title mb-20">
+                                <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/dca651144806585.6293d6cc100a6.png"
+                                    alt="MovieQart logo">
+                            </div>
+                            <div class="footer-link">
+                                We know you love watching your favorite actors; we think you should be more involved by
+                                rating, commenting and getting rewarded for the movies you watch.
+                            </div>
+                            <div class="f-contact mt-20">
+                                <ul>
+
+                                    <li>
+                                        <i class="icon dripicons-mail"></i> <span>Email : <a
+                                                href="mailto:hello@movieqart.com">hello@movieqart.com</a></span>
+                                    </li>
+
+                                </ul>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-sm-6">
+
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-sm-6">
+
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-sm-6">
+                        <div class="footer-widget mb-30">
+
+                            <div class="footer-social  mt-30">
+                                <a href="https://www.instagram.com/movieqartng" target="_blank"><i
+                                        class="fab fa-instagram"></i></a>
+                                <a href="https://www.facebook.com/movieqart" target="_blank"><i
+                                        class="fab fa-facebook-f"></i></a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="copyright-wrap">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        Copyright © 2022 MovieQart. All Rights Reserved.
+                    </div>
+                    <div class="col-lg-6 text-right text-xl-right">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer-end -->
+
+
+    <!-- JS here -->
+    <script src="{{ asset('js/vendor/modernizr-3.5.0.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/ajax-form.js') }}"></script>
+    <script src="{{ asset('js/paroller.js') }}"></script>
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <script src="{{ asset('js/js_isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('js/imagesloaded.min.js') }}"></script>
+    <script src="{{ asset('js/parallax.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.meanmenu.min.js') }}"></script>
+    <script src="{{ asset('js/parallax-scroll.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/element-in-view.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
